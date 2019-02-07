9f10901a400cdcdaa24eacb788b96c945c92990c
Daniel Newell <daniel.newell@amway.com>
2019-02-07 15:30:02 -0500

    Add assertion to test title and description

--- test/models/bug_test.rb
+++ test/models/bug_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class BugTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save bug without title" do
+    bug = Bug.new
+    assert_not bug.save, "Saved the bug without a title"
+  end
+
+  test "should not save bug without description" do
+    bug = Bug.new
+    assert_not bug.save, "Saved the bug without a description"
+  end
 end
